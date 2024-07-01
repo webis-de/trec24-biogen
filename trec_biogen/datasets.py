5debd896b9378f9195846d76fe412c6e22f78ef1
Jan Heinrich Reimer <heinrich.reimer@uni-jena.de>
2024-07-01 19:24:31 +0200

    Improve datasets abstraction

--- trec_biogen/datasets.py
+++ trec_biogen/datasets.py
@@ -1,29 +1,38 @@
-from typing import Any, Iterable, Literal, TypeAlias
+from typing import Any, Collection, Iterable, Literal, TypeAlias
+
+from ir_measures import Qrel, ScoredDoc
 from pydantic import BaseModel
 
 
 Split: TypeAlias = Literal["train", "dev", "test"]
 
 
-class RetrievalExample(BaseModel):
-    # TODO: What should correspond to the inputs and outputs of the generation module.
-    todo: Any
-
-
-RetrievalDataset: TypeAlias = Literal[
+RetrievalDatasetName: TypeAlias = Literal[
     "bioasq-task-b",
     # "trec-biogen",
 ]
 
 
-def load_retrieval_examples(
-    dataset: RetrievalDataset,
+class RetrievalDataset(BaseModel):
+    run: Iterable[ScoredDoc]
+    qrels: Iterable[Qrel]
+
+
+def load_retrieval_dataset(
+    dataset_name: RetrievalDatasetName,
     split: Split,
-) -> Iterable[RetrievalExample]:
+) -> RetrievalDataset:
     """
     Load retrieval examples for training, development, or testing.
     """
+    # TODO: Load dataset.
     return NotImplemented
+
+
+GenerationDatasetName: TypeAlias = Literal[
+    "bioasq-task-b",
+    # "trec-biogen",
+]
 
 
 class GenerationExample(BaseModel):
@@ -31,17 +40,16 @@ class GenerationExample(BaseModel):
     todo: Any
 
 
-GenerationDataset: TypeAlias = Literal[
-    "bioasq-task-b",
-    # "trec-biogen",
-]
+class GenerationDataset(BaseModel):
+    examples: Collection[GenerationExample]
 
 
-def load_generation_examples(
-    dataset: GenerationDataset,
+def load_generation_dataset(
+    dataset_name: GenerationDatasetName,
     split: Split,
-) -> Iterable[GenerationExample]:
+) -> GenerationDataset:
     """
     Load retrieval examples for training, development, or testing.
     """
+    # TODO: Load dataset.
     return NotImplemented
