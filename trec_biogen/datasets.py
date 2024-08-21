7200462dfd47f89686f31c6c503a99bf63302da0
Jan Heinrich Merker <heinrich.reimer@uni-jena.de>
2024-08-21 19:01:22 +0200

    Re-implement dataset loading/saving

--- trec_biogen/datasets.py
+++ trec_biogen/datasets.py
@@ -1,55 +1,87 @@
-from typing import Any, Collection, Iterable, Literal, TypeAlias
+from pathlib import Path
+from typing import Iterable, Sequence
 
-from ir_measures import Qrel, ScoredDoc
-from pydantic import BaseModel
+from pydantic.networks import EmailStr
+from tqdm import tqdm
 
-
-Split: TypeAlias = Literal["train", "dev", "test"]
-
-
-RetrievalDatasetName: TypeAlias = Literal[
-    "bioasq-task-b",
-    # "trec-biogen",
-]
-
-
-class RetrievalDataset(BaseModel):
-    run: Iterable[ScoredDoc]
-    qrels: Iterable[Qrel]
-
-
-def load_retrieval_dataset(
-    dataset_name: RetrievalDatasetName,
-    split: Split,
-) -> RetrievalDataset:
-    """
-    Load retrieval examples for training, development, or testing.
-    """
-    # TODO: Load dataset.
-    return NotImplemented
+from trec_biogen.model import (
+    Answer,
+    ClefBioAsqAnswer,
+    ClefBioAsqQuestion,
+    ClefBioAsqQuestions,
+    TrecBioGenQuestion,
+    TrecBioGenQuestions,
+    ClefBioAsqAnswers,
+    TrecBioGenAnswers,
+    Question,
+)
 
 
-GenerationDatasetName: TypeAlias = Literal[
-    "bioasq-task-b",
-    # "trec-biogen",
-]
+def load_clef_bioasq_questions(
+    path: Path,
+    progress: bool = False,
+) -> Sequence[Question]:
+    with path.open("rb") as file:
+        input = ClefBioAsqQuestions.model_validate_json(file.read())
+    questions: Iterable[ClefBioAsqQuestion] = input.questions
+    if progress:
+        questions = tqdm(questions, desc="Convert questions", unit="question")
+    return [question.as_question() for question in questions]
 
 
-class GenerationExample(BaseModel):
-    # TODO: What should correspond to the inputs and outputs of the generation module.
-    todo: Any
+def load_trec_biogen_questions(
+    path: Path,
+    progress: bool = False,
+) -> Sequence[Question]:
+    with path.open("rb") as file:
+        input = TrecBioGenQuestions.model_validate_json(file.read())
+    topics: Iterable[TrecBioGenQuestion] = input.topics
+    if progress:
+        topics = tqdm(topics, desc="Convert questions", unit="question")
+    return [question.as_question() for question in topics]
 
 
-class GenerationDataset(BaseModel):
-    examples: Collection[GenerationExample]
+def load_clef_bioasq_answers(
+    path: Path,
+    progress: bool = False,
+) -> Sequence[Answer]:
+    with path.open("rb") as file:
+        input = ClefBioAsqAnswers.model_validate_json(file.read())
+    questions: Iterable[ClefBioAsqAnswer] = input.questions
+    if progress:
+        questions = tqdm(questions, desc="Convert answers", unit="answer")
+    return [question.as_answer() for question in input.questions]
 
 
-def load_generation_dataset(
-    dataset_name: GenerationDatasetName,
-    split: Split,
-) -> GenerationDataset:
-    """
-    Load retrieval examples for training, development, or testing.
-    """
-    # TODO: Load dataset.
-    return NotImplemented
+def save_clef_bioasq_answers(
+    answers: Iterable[Answer],
+    path: Path,
+    progress: bool = False,
+) -> None:
+    if progress:
+        answers = tqdm(answers, desc="Convert answers", unit="answer")
+    output = ClefBioAsqQuestions(
+        questions=[answer.as_clef_bio_asq_answer() for answer in answers],
+    )
+    with path.open("wt", encoding="utf8") as file:
+        file.write(output.model_dump_json(indent=2))
+
+
+def save_trec_biogen_answers(
+    answers: Iterable[Answer],
+    team_id: str,
+    run_name: str,
+    contact_email: EmailStr,
+    path: Path,
+    progress: bool = False,
+) -> None:
+    if progress:
+        answers = tqdm(answers, desc="Convert answers", unit="answer")
+    output = TrecBioGenAnswers(
+        team_id=team_id,
+        run_name=run_name,
+        contact_email=contact_email,
+        results=[answer.as_trec_bio_gen_answer() for answer in answers],
+    )
+    with path.open("wt", encoding="utf8") as file:
+        file.write(output.model_dump_json(indent=2))
