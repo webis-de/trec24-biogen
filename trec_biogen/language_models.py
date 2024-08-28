b2753c6dff499a5a07976ac9ea0508f9809fb286
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2024-08-29 01:52:18 +0200

    Prepare Langchain language model

--- trec_biogen/language_models.py
+++ trec_biogen/language_models.py
@@ -1,7 +1,10 @@
 from os import environ
 from typing import Literal, Mapping, TypeAlias
 
-from dspy import LM, OpenAI as DSPyOpenAI
+from dspy import LM as DSPyLanguageModel, OpenAI as DSPyOpenAI
+from langchain_core.language_models import BaseLanguageModel as LangchainLanguageModel
+from langchain_openai.llms import OpenAI as LangchainOpenAI
+from pydantic.v1.types import SecretStr
 
 
 LanguageModelName: TypeAlias = Literal[
@@ -20,7 +23,7 @@ _BLABLADOR_MODEL_NAMES: Mapping[LanguageModelName, str] = {
 }
 
 
-def get_language_model(name: LanguageModelName) -> LM:
+def get_dspy_language_model(name: LanguageModelName) -> DSPyLanguageModel:
     if (
         name == "blablador:Mistral-7B-Instruct-v0.3" or
         name == "blablador:Mixtral-8x7B-Instruct-v0.1" or
@@ -33,6 +36,27 @@ def get_language_model(name: LanguageModelName) -> LM:
             model=blablador_name,
             api_key=environ["BLABLADOR_API_KEY"],
             api_base="https://helmholtz-blablador.fz-juelich.de:8000/v1/",
+        )
+    else:
+        raise ValueError(f"Unknown language model: {name}")
+
+
+
+
+
+def get_langchain_language_model(name: LanguageModelName) -> LangchainLanguageModel:
+    if (
+        name == "blablador:Mistral-7B-Instruct-v0.3" or
+        name == "blablador:Mixtral-8x7B-Instruct-v0.1" or
+        name == "blablador:GritLM-7B" or
+        name == "blablador:starcoder2-15b" or
+        name == "blablador:Llama3.1-8B-Instruct"
+    ):
+        blablador_name = _BLABLADOR_MODEL_NAMES[name]
+        return LangchainOpenAI(
+            model=blablador_name,
+            api_key=SecretStr(environ["BLABLADOR_API_KEY"]),
+            base_url="https://helmholtz-blablador.fz-juelich.de:8000/v1/",
         )
     else:
         raise ValueError(f"Unknown language model: {name}")
