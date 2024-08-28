9b83fbeecaf6f1c9cc3cbf07e35f3c8552da693b
Jan Heinrich Merker <heinrich.merker@uni-jena.de>
2024-08-29 01:07:17 +0200

    Add PyTerrier-based retrieval module

--- trec_biogen/retrieval.py
+++ trec_biogen/retrieval.py
@@ -1,11 +1,33 @@
 from dataclasses import dataclass
-from typing import Any, Sequence
+from typing import Hashable, Iterable, Sequence
 
-from pandas import DataFrame
+from pandas import DataFrame, Series
 from pyterrier.transformer import Transformer
+from tqdm.auto import tqdm
 
-from trec_biogen.model import PartialAnswer, RankedPubMedReference, RetrievalAnswer, Snippet
+from trec_biogen.model import (
+    PartialAnswer,
+    RankedPubMedReference,
+    RetrievalAnswer,
+    Snippet,
+    PubMedReferenceSentence,
+)
 from trec_biogen.modules import GenerationModule, RetrievalModule
+
+
+def _remove_dangling_references(
+    sentence: PubMedReferenceSentence,
+    references: Sequence[RankedPubMedReference],
+) -> PubMedReferenceSentence:
+    sentence_references = [
+        sentence_reference
+        for sentence_reference in sentence.references
+        if any(sentence_reference in reference for reference in references)
+    ]
+    return PubMedReferenceSentence(
+        sentence=sentence.sentence,
+        references=sentence_references,
+    )
 
 
 @dataclass(frozen=True)
@@ -14,23 +36,19 @@ class PyterrierRetrievalModule(RetrievalModule, Transformer):
     Retrieve relevant context using the PyTerrier framework.
     """
 
-    todo: Any  # TODO: Define hyper-parameters.
+    transformer: Transformer
+    progress: bool = False
 
     def transform(self, topics_or_res: DataFrame) -> DataFrame:
-        return NotImplemented
+        return self.transformer.transform(topics_or_res)
 
     def retrieve(self, context: PartialAnswer) -> RetrievalAnswer:
         answers = self.retrieve_many([context])
         return answers[0]
-    
-    @staticmethod
-    def _build_query(context: PartialAnswer) -> str:
-        # TODO: Build query.
-        return context.text
-    
-    @staticmethod
+
     def _parse_res(
-        context: PartialAnswer, 
+        self,
+        context: PartialAnswer,
         res: DataFrame | None,
     ) -> RetrievalAnswer:
         references: Sequence[RankedPubMedReference]
@@ -39,21 +57,34 @@ class PyterrierRetrievalModule(RetrievalModule, Transformer):
         else:
             references = [
                 RankedPubMedReference(
-                    pubmed_id=row["docno"],
-                    snippet=Snippet(
-                        text=row["text"],
-                        start_section=row["start_section"],
-                        start_offset=row["start_offset"],
-                        end_section=row["end_section"],
-                        end_offset=row["end_offset"],
+                    pubmed_id=row["docno"].split("%")[0],
+                    snippet=(
+                        Snippet(
+                            text=row["text"],
+                            start_section=row["start_section"],
+                            start_offset=row["start_offset"],
+                            end_section=row["end_section"],
+                            end_offset=row["end_offset"],
+                        )
+                        if row["text"]
+                        and row["start_section"]
+                        and row["start_offset"]
+                        and row["end_section"]
+                        and row["end_offset"]
+                        else None
                     ),
                     rank=row["rank"],
                 )
                 for _, row in res.iterrows()
             ]
 
+        # Remove dangling references from the summary.
         summary = context.summary
-        # TODO: Filter summary for missing references.
+        if summary is not None:
+            summary = [
+                _remove_dangling_references(sentence, references)
+                for sentence in summary
+            ]
 
         return RetrievalAnswer(
             id=context.id,
@@ -66,36 +97,44 @@ class PyterrierRetrievalModule(RetrievalModule, Transformer):
             references=references,
         )
 
-    def retrieve_many(self, contexts: Sequence[PartialAnswer]) -> Sequence[RetrievalAnswer]:
-        res: DataFrame = DataFrame([
-            {
-                "qid": qid,
-                "query": self._build_query(context)
-            }
-            for qid, context in enumerate(contexts)
-        ], columns=["qid", "query"])
+    def retrieve_many(
+        self, contexts: Sequence[PartialAnswer]
+    ) -> Sequence[RetrievalAnswer]:
+        res: DataFrame = DataFrame(
+            [
+                {
+                    "qid": qid,
+                    "context": context,
+                }
+                for qid, context in enumerate(contexts)
+            ],
+            columns=["qid", "context"],
+        )
         res = self.transform(res)
         res.sort_values(
             ["qid", "rank"],
-              ascending=[True,True], 
-              inplace=True,
+            ascending=[True, True],
+            inplace=True,
         )
         qid_res = {
-            int(qid): group # type: ignore
+            int(qid): group  # type: ignore
             for qid, group in res.groupby(
                 "qid",
-                  sort=False,
-                    group_keys=False,
+                sort=False,
+                group_keys=False,
             )
         }
-        return [
-            self._parse_res(
-                context=context,
-                res=qid_res[qid]
+        contexts_iterable: Iterable[PartialAnswer] = contexts
+        if self.progress:
+            contexts_iterable = tqdm(
+                contexts_iterable,
+                desc="Parse retrieved results",
+                unit="question",
             )
-            for qid, context in enumerate(contexts)
+        return [
+            self._parse_res(context=context, res=qid_res.get(qid))
+            for qid, context in enumerate(contexts_iterable)
         ]
-
 
 
 @dataclass(frozen=True)
